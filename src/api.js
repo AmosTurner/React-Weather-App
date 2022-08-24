56846458accf21fb7827301f7f62f7008ae130ac
AmosTurner <turneamo@sheridan.desire2learn.com>
2022-08-24 17:09:51 -0400

    city search component

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6e17aedf6bmsh736d5c82b81913ep1fb070jsn63d4e6946041',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
